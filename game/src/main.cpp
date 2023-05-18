f39e709c226ccac66b62aca7e2cc0ec8e6276a84
101191483 <101191483@WDF202IT05>
2023-05-18 12:17:43 -0400

    step 5 lab 2

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,16 +1,50 @@
 #include "rlImGui.h"
+#include"Math.h"
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
 int main(void)
 {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sunshine");
-    SetTargetFPS(60);
+    Vector2 position = { 100,500 };
+    Vector2 velocity = {10,0};
+    SetTargetFPS(144);
+    float maxSpeed = 1000;
 
+    rlImGuiSetup(true);// sets up imgui
+
+    HideCursor();
+
+    bool useGUI = false;
     while (!WindowShouldClose())
     {
+        const float dt =GetFrameTime();
+
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
+
+        if (IsKeyDown(KEY_GRAVE)) useGUI = !useGUI;
+        if (useGUI)
+        {
+            rlImGuiBegin();
+
+            ImGui::SliderFloat2("position", &(position.x), 0, SCREEN_WIDTH);
+            ImGui::SliderFloat2("velocity", &(velocity.x), -maxSpeed, maxSpeed);
+           
+
+            rlImGuiEnd();
+        }
+
+
+        
+        Vector2 displacement = velocity * dt;
+        position = position + displacement;
+
+        //position = GetMousePosition();
+        DrawCircleV(position, 50, DARKGREEN);
+
+        DrawFPS(1200, 10);
         DrawText("Hello World!", 16, 9, 20, RED);
         EndDrawing();
     }
