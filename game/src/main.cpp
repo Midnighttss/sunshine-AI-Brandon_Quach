6efb8176015566a23251ee073e5f95cd0bfef4eb
Brandon (Home) <Brandon.quach03@gmail.com>
2023-05-24 17:02:29 -0400

    fleeing???

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -99,12 +99,7 @@ int main(void)
             position = position - velocity * deltaTime - acceleration * deltaTime * deltaTime * 0.5f;
         }
         position = WraparoundScreen(position);
-        // Flee from another object in the world (e.g., a point)
-        /*if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
-
-        Vector2 fleeTargetPosition = { 500, 300 };
-        Vector2 fleeAcceleration = flee(target, velocity, fleeTargetPosition, speed);
-        }*/
+        
         DrawText("Hello World!", 16, 9, 20, RED);
 
         DrawCircleV(position, 50, GREEN);
