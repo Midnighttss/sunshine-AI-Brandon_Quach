e2692493dd60dd02cbb91ba21a310c04b64c70fe
Brandon (Home) <Brandon.quach03@gmail.com>
2023-06-06 21:40:35 -0400

    finished lab 3

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -16,7 +16,6 @@ struct Rigidbody
     Vector2 pos{};
     Vector2 vel{};
     Vector2 acc{};
-
     Vector2 dir{};
     float angularSpeed;
 };
@@ -45,21 +44,27 @@ public:
         : lineLength(lineLength), radius(radius)
     {
         rb.pos = { startX, startY };
-        rb.dir = { 0.0f, 1.0f };
+        rb.dir = { 0.0, 1.0 };
         rb.angularSpeed = angularSpeed;
     }
 
     void ObstacleAvoidance(const Vector2& obstaclePosition, float obstacleRadius, float avoidanceForce, float deltatime)
     {
-        Vector2 right = Rotate(rb.dir, 15 * DEG2RAD);
-        Vector2 left = Rotate(rb.dir, -15 * DEG2RAD);
+        Vector2 right = Rotate(rb.dir, 90 * DEG2RAD);
+        Vector2 left = Rotate(rb.dir, -90 * DEG2RAD);
+        Vector2 top = Rotate(rb.dir, 0 * DEG2RAD);
+        Vector2 bottom = Rotate(rb.dir, 180 * DEG2RAD);
         Vector2 rightWisk = rb.pos + right * lineLength;
         Vector2 leftWisk = rb.pos + left * lineLength;
+        Vector2 topWisk = rb.pos + top * lineLength;
+        Vector2 bottomWisk = rb.pos + bottom * lineLength;
 
         bool leftCollision = CheckCollisionLineCircle(rb.pos, leftWisk, obstaclePosition, obstacleRadius);
         bool rightCollision = CheckCollisionLineCircle(rb.pos, rightWisk, obstaclePosition, obstacleRadius);
+        bool topCollision = CheckCollisionLineCircle(rb.pos, topWisk, obstaclePosition, obstacleRadius);;
+        bool bottomCollision = CheckCollisionLineCircle(rb.pos, bottomWisk, obstaclePosition, obstacleRadius);;
 
-        if (leftCollision || rightCollision)
+        if (leftCollision || rightCollision||topCollision||bottomCollision)
         {
             Vector2 avoidanceDir = Normalize(rb.pos - obstaclePosition);
             rb.acc = rb.acc + avoidanceDir * avoidanceForce;
@@ -78,7 +83,18 @@ public:
             float linearSpeed = Length(rb.vel);
             rb.vel = Rotate(linearDirection, -rb.angularSpeed * deltatime * DEG2RAD) * linearSpeed;
         }
-
+        if (topCollision)
+        {
+            Vector2 linearDirection = Normalize(rb.vel);
+            float linearSpeed = Length(rb.vel);
+            rb.vel = Rotate(linearDirection, -rb.angularSpeed * deltatime * DEG2RAD) * linearSpeed;
+        }
+        if (bottomCollision)
+        {
+            Vector2 linearDirection = Normalize(rb.vel);
+            float linearSpeed = Length(rb.vel);
+            rb.vel = Rotate(linearDirection, -rb.angularSpeed * deltatime * DEG2RAD) * linearSpeed;
+        }
     }
 };
 
@@ -125,22 +141,37 @@ int main(void)
             DrawCircleV(obstaclePosition, 25, BLUE);
         }
         
-        Vector2 right = Rotate(seeker.rb.dir, 15.0 * DEG2RAD);
-        Vector2 left = Rotate(seeker.rb.dir, -15.0 * DEG2RAD);
+        Vector2 right = Rotate(seeker.rb.dir, 90.0 * DEG2RAD);
+        Vector2 left = Rotate(seeker.rb.dir, -90.0 * DEG2RAD);
+        Vector2 top = Rotate(seeker.rb.dir, 0 * DEG2RAD);
+        Vector2 bottom = Rotate(seeker.rb.dir, 180 * DEG2RAD);
+
         Vector2 rightWisk = seeker.rb.pos + right * seekerWiskerLength;
         Vector2 leftWisk = seeker.rb.pos + left * seekerWiskerLength;
+        Vector2 topWisk = seeker.rb.pos + top * seekerWiskerLength;
+        Vector2 bottomWisk = seeker.rb.pos + bottom * seekerWiskerLength;
+        
         bool leftCollision = false;
         bool rightCollision = false;
+        bool topCollision = false;
+        bool bottomCollision = false;
 
         for (const auto& obstaclePosition : obstaclePositions)
         {
             leftCollision = CheckCollisionLineCircle(seeker.rb.pos, leftWisk, obstaclePosition, obstacleRadius);
             rightCollision = CheckCollisionLineCircle(seeker.rb.pos, rightWisk, obstaclePosition, obstacleRadius);
+            topCollision = CheckCollisionLineCircle(seeker.rb.pos, topWisk, obstaclePosition, obstacleRadius);
+            bottomCollision = CheckCollisionLineCircle(seeker.rb.pos, bottomWisk, obstaclePosition, obstacleRadius);
         }
         Color rightColor = rightCollision ? RED : GREEN;
         Color leftColor = leftCollision ? RED : GREEN;
+        Color topColor = leftCollision ? RED : GREEN;
+        Color bottomColor = leftCollision ? RED : GREEN;
+
         DrawLineV(seeker.rb.pos, rightWisk, rightColor);
         DrawLineV(seeker.rb.pos, leftWisk, leftColor);
+        DrawLineV(seeker.rb.pos, topWisk, topColor);
+        DrawLineV(seeker.rb.pos, bottomWisk, bottomColor);
         
 
 
